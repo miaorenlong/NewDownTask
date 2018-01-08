1de65a0fb17a19ca7b4ea757d83c698dbaa52d96
xiacaixiang <xiacaixiang@touzila.com>
2018-01-08 11:23:37 +0800

    第一次提交

---
+++ NewDownLoadTask/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NewDownLoadTask
+//
+//  Created by 夏财祥 on 2018/1/8.
+//  Copyright © 2018年 众鑫贷. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ NewDownLoadTaskTests/NewDownLoadTaskTests.m
@@ -0,0 +1,39 @@
+//
+//  NewDownLoadTaskTests.m
+//  NewDownLoadTaskTests
+//
+//  Created by 夏财祥 on 2018/1/8.
+//  Copyright © 2018年 众鑫贷. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NewDownLoadTaskTests : XCTestCase
+
+@end
+
+@implementation NewDownLoadTaskTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
